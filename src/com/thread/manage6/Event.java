8754ded5fbd988fcf638d5ecc35bc2fc0f43a898
elliott <passerby660@outlook.com>
2020-12-28 00:24:47 +0800

    守护线程的创建与运行

---
+++ src/com/thread/manage6/Event.java
@@ -0,0 +1,31 @@
+package com.thread.manage6;
+
+import java.util.Date;
+
+public class Event {
+    private Date date;
+    private String event;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public Event(){}
+
+    public Event(Date date, String event) {
+        this.date = date;
+        this.event = event;
+    }
+}
